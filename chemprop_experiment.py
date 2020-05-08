20da88cfb6da368ca22a45e8036883cdfe38c12b
dklink <dougdklink3@gmail.com>
2020-05-08 14:10:18 -0400

    got data running through chemprop

--- chemprop_experiment.py
+++ chemprop_experiment.py
@@ -3,5 +3,9 @@ import glob
 import os
 
 csvs = glob.glob(os.path.join('data', '*.csv'))
-df = pd.concat((pd.read_csv(f) for f in csvs))
+raw_data = pd.concat((pd.read_csv(f) for f in csvs))
 
+chemprop_data = raw_data[['SMILES', 'Activity']]
+chemprop_data.to_csv('chemprop_in.csv', index=False)
+
+os.system('python chemprop/train.py --data_path chemprop_in.csv --dataset_type regression --save_dir models')
